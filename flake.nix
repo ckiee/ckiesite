556885692138ec3bae3931fab25e1ff72cd9e05b
ckie <git-525ff67@ckie.dev>
2022-06-24 20:31:54 +0300

    nix: add flake-compat

--- flake.nix
+++ flake.nix
@@ -2,32 +2,32 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = nixpkgs.legacyPackages."${system}";
         naersk-lib = naersk.lib."${system}";
-      in
-        rec {
-          # `nix build`
-          packages.hello-world = naersk-lib.buildPackage {
-            pname = "hello-world";
-            root = ./.;
-          };
-          defaultPackage = packages.hello-world;
+      in rec {
+        # `nix build`
+        packages.ckiesite = naersk-lib.buildPackage {
+          pname = "ckiesite";
+          root = ./.;
+        };
+        defaultPackage = packages.ckiesite;
 
-          # `nix run`
-          apps.hello-world = flake-utils.lib.mkApp {
-            drv = packages.hello-world;
-          };
-          defaultApp = apps.hello-world;
+        # `nix run`
+        apps.ckiesite =
+          flake-utils.lib.mkApp { drv = packages.ckiesite; };
+        defaultApp = apps.ckiesite;
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ rustc cargo ];
-          };
-        }
-    );
+        # `nix develop`
+        devShell =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
+      });
 }
