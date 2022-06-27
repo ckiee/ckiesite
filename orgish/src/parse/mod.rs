1e88beb5c31f13d97b604aa44476792cbd80caa2
ckie <git-525ff67@ckie.dev>
2022-06-27 20:58:05 +0300

    orgish#parse: better errors

--- orgish/src/parse/mod.rs
+++ orgish/src/parse/mod.rs
@@ -21,6 +21,53 @@ pub fn parse_n_pass(input: &str) -> Result<PassedSyntaxTree> {
             &mut ast.iter().peekable(),
             pass1::StopAt::Eof,
         )?)?),
-        Err(pain) => Err(anyhow!(pain.to_string())),
+        Err(pain) => {
+            let pos = pain.position;
+            let line_range: usize = 3;
+
+            let org_syntax = SyntaxDefinition::load_from_str(include_str!("../org.sublime-syntax"), true, None)?;
+            let ps = {
+                let mut b = SyntaxSetBuilder::new();
+                b.add(org_syntax);
+                b.build()
+            };
+
+            let ts = ThemeSet::load_defaults();
+            let hl_syntax = ps.find_syntax_by_extension("org").unwrap();
+            let mut highlighter = HighlightLines::new(hl_syntax, &ts.themes["base16-ocean.dark"]);
+
+            let src: String = input
+                .lines()
+                .skip((pos.line as usize) - line_range + 1)
+                .take(line_range)
+                .enumerate()
+                .map(|(idx, line)| {
+                    let line_with_ending = format!("{line}\n");
+                    let highlighted_line = highlighter.highlight(&line_with_ending, &ps);
+                    let term_line = as_24_bit_terminal_escaped(&highlighted_line[..], true);
+
+                    if idx == line_range / 2 {
+                        format!("{term_line}----^\n")
+                    } else {
+                        term_line
+                    }
+                })
+                .collect();
+
+            let pain_str = pain.to_string();
+            let mut pain_lines = pain_str.lines();
+            let pain_header = pain_lines
+                .next()
+                .expect("the error message to always have content");
+            let clear_color = "\x1b[0m";
+            let rest_of_pain = pain_lines.intersperse("\n").collect::<String>();
+
+            Err(anyhow!(format!(
+                r#"
+{pain_header}:
+{src} {clear_color}
+{rest_of_pain}"#
+            )))
+        }
     }
 }
